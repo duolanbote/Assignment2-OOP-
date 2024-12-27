4d4cbedaf817f7b6611fc6057e6e94aef6d4c691
MAX <153761554+duolanbote@users.noreply.github.com>
2024-12-27 17:58:10 +0800

    Update Assignment2.py

--- Assignment2.py
+++ Assignment2.py
@@ -10,22 +10,57 @@ menu1 = {
 }
 
 class Memu():
-    def __init__(self, option):
-        self.option = option
+    def __init__(self, title, option):
+        self._title = title
+        self._option = option
         pass
-    
-    def getmenu(self):
-        num = 1
-        for i in range(len(self.option)):
-            print(num + '.' + self.option)
-            num += 1
-            pass
-    def menuselection(self):
-        select = int(input('Please enter a number:'))
-        if select < 1 or select > 3:
-            print('Wrong input, must be a number between 1 and 3')
-            select = int(input('Please enter a number:'))
-        else:
-            print()
 
+
+    @property
+    def title(self):
+        return self._title
+    @title.setter
+    def title(self, set_title):
+        self._title = set_title
+        pass
+
+
+    @property
+    def option(self):
+        return self._option
+    @option.setter
+    def option(self, option_selection):
+        self._option = option_selection
+        pass
+
+    @property
+    def choice(self):
+        return self._choice
+    @choice.setter
+    def choice(self, choiceselection):
+        self._choice = int(choiceselection)
+        pass
+
+    def input(self):
+        user_input = input("Please enter a number: ")
+        self.choice = user_input
+        return self.choice
+        
+    def menu():
+        print('Home menu')
+        print('1. COMPONENTS')
+        print('2. CIRCUIT KITS')
+        print('3. CLOSE')
+        choice = input('Please enter a number')
+
+        if choice == '1':
+            menu1()
+        elif choice == '2':
+            menu2()
+    
+
+
+
+
+    
 #12
