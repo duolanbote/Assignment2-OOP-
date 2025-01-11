2dee0a7c771345d124657062edffbf56a131923f
Winky27 <winkylam725@gmail.com>
2025-01-11 22:58:55 +1030

    16

--- component.py
+++ component.py
@@ -215,4 +215,40 @@ class Sensor(InputComponent):
     
 class OutputComponent(Component, ABC):
     pass
-                      
+
+class Light(OutputComponent, ABC):
+    def __init__(self, name: str, price: float, lumens: float):
+        super().__init__(name, price)
+        self._lumens = lumens
+
+    @property
+    def lumens(self) -> float:
+        return self._lumens
+
+    @lumens.setter
+    def lumens(self, value: float):
+        if isinstance(value, (int, float)) and value > 0:
+            self._lumens = float(value)
+        else:
+            raise ValueError("Lumens must be a positive number")
+
+class LightGlobe(Light):
+    def __init__(self, name: str, price: float, lumens: float, wattage: float):
+        super().__init__(name, price, lumens)
+        self._wattage = wattage  
+
+    @property
+    def wattage(self) -> float:
+        return self._wattage
+
+    @wattage.setter
+    def wattage(self, value: float):
+        if isinstance(value, (int, float)) and value > 0:
+            self._wattage = float(value)
+        else:
+            raise ValueError("Wattage must be a positive number")
+
+    @classmethod
+    def parse_csv(cls, csv_string: str) -> 'LightGlobe':
+        name, price, lumens, wattage = csv_string.split(',')
+        return cls(name, float(price), float(lumens), float(wattage))
