9032c42d863d7f0cbd934037d3301238b349a8f7
Winky27 <winkylam725@gmail.com>
2025-01-14 04:49:38 +1030

    20

---
+++ circuit.py
@@ -0,0 +1,11 @@
+class Circuit:
+    def __init__(self):
+        self.components = [] 
+    
+    def parse_csv(self, csv_string):
+        self.components = []
+        lines = csv_string.strip().split('\n')
+        for line in lines:
+            attributes = line.split(',')
+            component = Component(*attributes)
+            self.components.append(component)
