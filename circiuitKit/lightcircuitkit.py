0a6e36112f83981663672d599474ddd6daf511df
MAX <2638995179lyx@gmail.com>
2025-01-16 01:10:29 +1030

    max

--- circiuitKit/lightcircuitkit.py
+++ circiuitKit/lightcircuitkit.py
@@ -1,8 +1,34 @@
 from .circuit import CircuitKit
 
 class LightCircuitKit(CircuitKit):
-    
+    def __init__(self, name: str):
+        self.name = name
+        self.components = []
 
-    def validate_circuit(self):
-        
-        return len(self.components) >= 2
+    def add_component(self, component):
+        self.components.append(component)
+
+    def remove_component(self, component):
+        if component in self.components:
+            self.components.remove(component)
+
+    def display_lights(self):
+        print(f"Lights in '{self.name}':")
+        found_lights = False
+        for component in self.components:
+            if component.__class__.__name__ in ["LightGlobe", "LEDLight"]:
+                print(component.display())
+                found_lights = True
+        if not found_lights:
+            print("No lights in the circuit.")
+
+    def check_functioning(self):
+        has_light = False
+        has_power = False
+        for component in self.components:
+            if component.__class__.__name__ in ["LightGlobe", "LEDLight"]:
+                has_light = True
+            if component.__class__.__name__ in ["Battery", "SolarPanel"]:
+                has_power = True
+        return has_light and has_power
+
--- circiuitKit/sensorcircuitkit.py
+++ circiuitKit/sensorcircuitkit.py
@@ -1,8 +1,33 @@
 from .circuit import CircuitKit
 
 class SensorCircuitKit(CircuitKit):
-    
+    def __init__(self, name: str):
+        self.name = name
+        self.components = []
 
-    def validate_circuit(self):
-        
-        return len(self.components) >= 2
+    def add_component(self, component):
+        self.components.append(component)
+
+    def remove_component(self, component):
+        if component in self.components:
+            self.components.remove(component)
+
+    def display_sensors(self):
+        print(f"Sensors in '{self.name}':")
+        found_sensors = False
+        for component in self.components:
+            if component.__class__.__name__ == "Sensor":
+                print(component.display())
+                found_sensors = True
+        if not found_sensors:
+            print("No sensors in the circuit.")
+
+    def check_functioning(self):
+        has_sensor = False
+        has_power = False
+        for component in self.components:
+            if component.__class__.__name__ == "Sensor":
+                has_sensor = True
+            if component.__class__.__name__ in ["Battery", "SolarPanel"]:
+                has_power = True
+        return has_sensor and has_power
